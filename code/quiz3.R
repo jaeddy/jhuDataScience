29e86e0fa8b505e224a67c261c852cf0fc98a5a5
jaeddy <james.a.eddy@gmail.com>
2014-05-20 13:32:12 -0400

    initial commit for quiz 3

---
+++ code/quiz3.R
@@ -0,0 +1,42 @@
+
+# Code to obtain solutions to Quiz 1 problems
+
+## Question 1: Take a look at the 'iris' dataset that comes with R. The data 
+## can be loaded with the code:
+library(datasets)
+data(iris)
+
+## A description of the dataset can be found by running
+?iris
+
+## There will be an object called 'iris' in your workspace. In this dataset, 
+## what is the mean of 'Sepal.Length' for the species virginica? 
+## (Please only enter the numeric result and nothing else.)
+
+
+## Question 2: Continuing with the 'iris' dataset from the previous Question, 
+## what R code returns a vector of the means of the variables 'Sepal.Length', 
+## 'Sepal.Width', 'Petal.Length', and 'Petal.Width'?
+
+
+## Question 3: Load the 'mtcars' dataset in R with the following code
+library(datasets)
+data(mtcars)
+
+## There will be an object names 'mtcars' in your workspace. You can find some 
+## information about the dataset by running
+?mtcars
+
+## How can one calculate the average miles per gallon (mpg) by number of 
+## cylinders in the car (cyl)?
+
+
+## Question 4: Continuing with the 'mtcars' dataset from the previous Question, 
+## what is the absolute difference between the average horsepower of 4-cylinder 
+## cars and the average horsepower of 8-cylinder cars?
+
+
+## Question 5: If you run
+debug(ls)
+
+## what happens when you next call the 'ls' function?
