e94be27a62a5c838c83fa85813ae100bd487231e
jaeddy <james.a.eddy@gmail.com>
2014-05-25 12:48:28 -0700

    code finished, tested, & commented

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,86 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Brief overviews of what each function does are found below (preceeding 
+## individual functions)
 
-## This function creates a special "matrix" object that can cache its inverse
+## More detailed comments describing smaller steps are included in the function;
+## although some steps could be written in a single line, all functions are
+## written in out in full multi-line form (with spacing between individual 
+## functions) for the sake of clarity
+
+
+## This function creates a special "matrix" object that can cache its inverse. 
+## Specifically, makeCacheMatrix creates a list with a function to (1) set the 
+## value of the matrix; (2) get the value of the matrix; (3) set the value of 
+## the inverse of the matrix; (4) get the value of the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## initialize the value of the matrix inverse 'inv' to NULL
+    inv <- NULL
+    
+    ## set the value of the matrix 'x' equal to the input matrix 'y'; set the 
+    ## value of the inverse matrix to NULL to clear the cache when 'x' is 
+    ## changed
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    ## simply return the value of 'x'
+    get <- function() {
+        x
+    }
+    
+    ## set the value of 'inv' equal to the calculated value of the inverse 
+    ## matrix; note: this value is not calculated by the setinv function
+    setinv <- function(inverse) {
+        inv <<- inverse
+    }
+    
+    ## return the value of the matrix inverse 'inv'
+    getinv <- function() {
+        inv
+    }
+    
+    ## return a list with each of the above functions representing the 
+    ## "special" matrix
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
 ## This function computes the inverse of the special "matrix" returned by 
-## makeCacheMatrix above. If the inverse has already been calculated (and the 
-## matrix has not changed), then cacheSolve should retrieve the inverse from 
-## the cache.
+## makeCacheMatrix above. cacheSolve first checks whether the inverse has 
+## already been calculated using the getinv() function within the object
+## defined by makeCacheMatrix; if so, the cacheSolve retrieves the inverse from 
+## the cache and prints a message.
+
+## If the inverse of the matrix has not been calculated (its value is NULL),
+## then the solve() function is used to calculate the inverse; this value is 
+## then returned by cacheSolve.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## set the value of the matrix inverse 'inv' equal to the stored matrix
+    ## inverse value for 'x'
+    inv <- x$getinv()
+    
+    ## if the value of 'inv' is not NULL, this indicates that the inverse of 'x'
+    ## has already been calculated and cached; in this case, return 'inv' and 
+    ## exit the function
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    
+    ## if 'inv' is NULL, then the inverse of 'x' has not been cached and must be
+    ## calculated; set 'data' equal to the value of 'x'
+    data <- x$get()
+    
+    ## calculate the inverse of 'data' using the solve() function
+    inv <- solve(data, ...)
+    
+    ## cache the newly calculated inverse matrix 
+    x$setinv(inv)
+    
+    ## return a matrix that is the inverse of 'x'
+    inv 
 }
--- cachevector.R
+++ cachevector.R
@@ -1,3 +1,7 @@
+## The first function, makeVector creates a special "vector", which is really a 
+## list containing a function to (1) set the value of the vector; (2) get the 
+## value of the vector; (3) set the value of the mean; (4) get the value of the 
+## mean
 
 makeVector <- function(x = numeric()) {
     m <- NULL
@@ -12,6 +16,12 @@ makeVector <- function(x = numeric()) {
          setmean = setmean,
          getmean = getmean)
 }
+
+## The following function calculates the mean of the special "vector" created 
+## with the above function. However, it first checks to see if the mean has 
+## already been calculated. If so, it gets the mean from the cache and skips the
+## computation. Otherwise, it calculates the mean of the data and sets the value
+## of the mean in the cache via the setmean function.
 
 cachemean <- function(x, ...) {
     m <- x$getmean()
