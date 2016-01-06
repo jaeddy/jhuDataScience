22a88c3f642b5a3b1afe1c65ee7b212fded1bb37
James Eddy <james.a.eddy@gmail.com>
2016-01-06 11:11:08 -0800

    removing packrat folders

--- mathematicalBiostatistics/packrat/lib/x86_64-apple-darwin13.1.0/3.1.1/packrat/tests/testthat/test-hash.R
+++
@@ -1,8 +0,0 @@
-context("hash")
-
-test_that("we can hash packages containing multiple packages in LinkingTo", {
-  path <- system.file("DESCRIPTION", package = "Rclusterpp")
-  if (file.exists(path)) {
-    hash(path)
-  }
-})
